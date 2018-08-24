e0a14bd55b542d4ce328db5ac7d85488e0a60d2b
unknown <dreinwaldmail@gmail.com>
2018-08-23 19:46:05 -0700

    some tinkering with prog bar

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,35 @@ $(document).ready(function(){
     var correctAnswer = 0;
     var WrongAnswer = 0;
     var timer = 60;
-    var 
+    
 
 
 
 
+    //timer operations
+    $("#start").on("click", downloadTimer);
+   
+
+    
 
 
+
+
+    
+    
+    
+    
+    // Generic function that ties button click action to page. allowing doc to see if button has desired class then run function associated.
+    $(document).on("click", ".class", functiontodo);
+    
+    
+    
+    //progress bar increase timer
+    // var downloadTimer = setInterval(function(){d
+    //     document.getElementById("progressBar").value = 60 - --timer;
+    //     if(timer <= 0)
+    //       clearInterval(downloadTimer);
+    //   },1000);
 
 
 
